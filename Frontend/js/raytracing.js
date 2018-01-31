f0ff41851887b29f218698b07cb26ac5d0807ea6
chiknas <35897259+chiknas@users.noreply.github.com>
2018-01-31 00:55:45 +0000

    Initial design for the Frontend

---
+++ Frontend/js/raytracing.js
@@ -0,0 +1,138 @@
+function addShape() {
+	
+	
+	
+		//get the canvas
+		var c = document.getElementById("myCanvas");
+		var ctx = c.getContext("2d");
+		
+		
+		//get shape
+		var e = document.getElementById("shape");
+		var shape = e.options[e.selectedIndex].value;
+		
+		//get coordinates(circle center, cube upper left corner
+		var x = parseInt(document.getElementById('shape_x').value);
+		var y = parseInt(document.getElementById('shape_y').value);
+		y = -y + c.height/2;
+		x = x + c.width/2;
+		
+		
+		//get size and color
+		var size = parseInt(document.getElementById('size').value);
+		var color = document.getElementById('color').value;
+		
+		
+		//paint the shape
+		if(shape == "Circle"){
+			ctx.beginPath();
+			ctx.arc(x,y,size,0,2*Math.PI);
+			ctx.fillStyle = color;
+			ctx.fill();
+			ctx.stroke();
+		
+		}else if(shape == "Cube"){
+			
+			//awesome cube code I made myself and the team doesnt need
+			
+			ctx.beginPath();
+			ctx.rect(x, y, size, size);
+			ctx.fillStyle = color;
+			ctx.fill();
+			ctx.stroke();
+			ctx.closePath();
+			
+			ctx.beginPath();
+			ctx.moveTo(x, y);
+			ctx.lineTo(x + (size / 2 ), y - (size / 2));			
+			x = x + (size / 2 );
+			y = y - (size / 2);
+			ctx.lineTo(x + size, y );			
+			x = x + size;
+			ctx.lineTo(x - (size / 2 ), y + (size / 2) );			
+			ctx.closePath();
+			ctx.fillStyle = color;
+			ctx.stroke();
+			ctx.fill();
+			
+			
+			ctx.beginPath();
+			ctx.moveTo(x, y);
+			ctx.lineTo(x , y + size);
+			
+			
+			
+			y = y + size;
+			ctx.lineTo(x - size/2 , y + size/2);
+			
+			x = x - size/2;
+			y = y + size/2;
+			ctx.lineTo(x, y - size);
+			ctx.closePath();
+			ctx.fillStyle = color;
+			ctx.stroke();
+			ctx.fill();
+			
+			
+		}
+	
+		
+	}
+	
+function clearGrid() {
+	var c = document.getElementById("myCanvas");
+	var ctx = c.getContext("2d");
+	ctx.clearRect(0, 0, c.width, c.height);
+}
+
+function gridSize() {
+	var v = document.getElementById("screenSize").value;
+	if(v == '1'){
+		document.getElementById("center").style.backgroundSize = "10% 10%";
+	}else if(v == '2'){
+		document.getElementById("center").style.backgroundSize = "9% 9%";
+	}else if(v == '3'){
+		document.getElementById("center").style.backgroundSize = "8% 8%";
+	}else if(v == '4'){
+		document.getElementById("center").style.backgroundSize = "7% 7%";
+	}else if(v == '5'){
+		document.getElementById("center").style.backgroundSize = "6% 6%";
+	}else if(v == '6'){
+		document.getElementById("center").style.backgroundSize = "5% 5%";
+	}
+	
+	
+	
+	
+	
+	
+	
+}
+
+
+function cartToScreen(py) {
+  return -py + HEIGHT;
+};	
+
+
+	//setup page
+  $(document).ready(function() {
+    
+    $('#picker').farbtastic('#color');
+	
+	//fit canvas to div
+	var canvas = document.getElementById("myCanvas");
+	fitToContainer(canvas);
+
+	function fitToContainer(canvas){
+		// Make it visually fill the positioned parent
+		canvas.style.width ='100%';
+		canvas.style.height='100%';
+		// ...then set the internal size to match
+		canvas.width  = canvas.offsetWidth;
+		canvas.height = canvas.offsetHeight;
+	}
+
+  });
+  
+  
