d673e5df5600bb0679223e041910d249dfb5d70a
Sebastian Köhler <sebastiankoehler2@gmx.net>
2018-02-04 11:12:38 +0000

    added deserialization for raytracing task

--- backend/RayTracing/Classes/Utils/JSONParser.py
+++ backend/RayTracing/Classes/Utils/JSONParser.py
@@ -1,16 +1,25 @@
+from RayTracing.Classes.Models.AmbientLight import AmbientLight
+from RayTracing.Classes.Models.Camera import Camera
 from RayTracing.Classes.Models.Imageplane import Imageplane
 from RayTracing.Classes.Models.Light import Light
 from RayTracing.Classes.Models.Scene import Scene
 from RayTracing.Classes.Models.Sphere import Sphere
 from RayTracing.Classes.Models.Vector import Vector
+from RayTracing.Classes.RayTracer import RayTracer
 
 
 class JSONParser:
 
+    def deserializeRayTracingTask(self, json):
+        imageplane = self.createImageplane(json)
+        camera = Camera()
+        scene = self.deserializeScene(self, json)
+
+        return RayTracer(self, imageplane, scene, camera)
+
+
     def deserializeScene(self, json):
         scene = Scene()
-
-        imageplane = self.createImageplane(json)
 
         jsonScene = json["scene"]
 
@@ -23,6 +32,8 @@ class JSONParser:
 
         for light in jsonScene["Light"]:
             scene.addLight(self.deserializeLight(light))
+
+        scene.addLight(self.deserializeAmbientLight(json["AmbientLight"]))
 
         return scene
 
@@ -62,3 +73,12 @@ class JSONParser:
         sideLength = cubeJson["sideLength"]
 
         return Cube(center, sideLength, reflection)'''
+
+
+    def deserializeAmbientLight(self, ambientLightJson):
+        active = ambientLightJson["active"]
+        brightness = 0
+        if(active == True):
+            brightness = ambientLightJson["brightnesse"]
+
+        return AmbientLight(self, brightness)
