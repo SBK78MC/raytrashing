7956ea55a383aafc5bea6b29581ace24a8618476
StStampelos <stamatis_x3@hotmail.com>
2018-02-11 18:40:16 +0000

    Add files via upload
    Added test for diffusion

--- backend/RayTracing/Tests/test_shading.py
+++ backend/RayTracing/Tests/test_shading.py
@@ -68,12 +68,72 @@ class MyTestCase(unittest.TestCase):
         arrColor = raytracer.getColorForIntersection(sphereIntersection)
         testColor = Color(arrColor[0], arrColor[1], arrColor[2])
 
-        print(arrColor)
+
         green = Color()
         green.green()
 
         testValue = testColor.isBrighterOrEqualTo(green)
         self.assertFalse(testValue)
+
+    def test_diffusion1(self):
+        #Light
+        light = Light(0, 0, 0, 1)
+
+        # Sphere 1
+        c1 = Vector(0, 0, 12)
+        s1 = Sphere(c1, 1, Color(0, 1, 0), 1000)
+
+        scene = Scene()
+        scene.addObject3D(s1)
+        scene.addLight(light)
+
+        camera = Camera(Vector(0, 0, 0), Vector(0, 0, 1), 30)
+
+        imagepl = Imageplane(500, 500)
+
+        raytracer = RayTracer(imagepl, scene, camera)
+
+        pixelRay1 = Ray(camera.position, Vector(0.001, 0, 1))
+
+        pixelRay2 = Ray(camera.position, Vector(0.01, 0, 1))
+
+        pixelRay3 = Ray(camera.position, Vector(0.02, 0, 1))
+
+        pixelRay4 = Ray(camera.position, Vector(0.03, 0, 1))
+
+        sphereIntersection1 = s1.intersection(pixelRay1)
+
+        sphereIntersection2 = s1.intersection(pixelRay2)
+
+        sphereIntersection3 = s1.intersection(pixelRay3)
+
+        sphereIntersection4 = s1.intersection(pixelRay4)
+
+        arrColor1 = raytracer.getColorForIntersection(sphereIntersection1)
+
+        arrColor2 = raytracer.getColorForIntersection(sphereIntersection2)
+
+        arrColor3 = raytracer.getColorForIntersection(sphereIntersection3)
+
+        arrColor4 = raytracer.getColorForIntersection(sphereIntersection4)
+
+        testColor1 = Color(arrColor1[0], arrColor1[1], arrColor1[2])
+
+        testColor2 = Color(arrColor2[0], arrColor2[1], arrColor2[2])
+
+        testColor3 = Color(arrColor3[0], arrColor3[1], arrColor3[2])
+
+        testColor4 = Color(arrColor4[0], arrColor3[1], arrColor3[2])
+
+
+        testValue3 = testColor3.isBrighterOrEqualTo(testColor4)
+        self.assertTrue(testValue3)
+
+        testValue2 = testColor2.isBrighterOrEqualTo(testColor3)
+        self.assertTrue(testValue2)
+
+        testValue1 = testColor1.isBrighterOrEqualTo(testColor2)
+        self.assertTrue(testValue1)
 
 
 if __name__ == '__main__':
