325a8bd62f6dc649e3ef973f5fdf47d64da5070c
Sebastian Köhler <sebastiankoehler2@gmx.net>
2018-03-05 22:33:38 +0000

    added surface normal

--- backend/RayTracing/Classes/Models/Cube.py
+++ backend/RayTracing/Classes/Models/Cube.py
@@ -6,14 +6,14 @@ from RayTracing.Classes.Models.Vector import Vector
 
 class Cube(Cuboid):
 
-    def __init__(self, x=0, y=0, z=0, l=0, color=Color(), reflection=10):
-        super().__init__(x, y, z, l, l, l, color, reflection)
+    def __init__(self, x=0, y=0, z=0, l=0, color=Color(), specular=100, reflection=0.1):
+        super().__init__(x, y, z, l, l, l, color, specular, reflection)
 
 
-    def __init__(self, v=Vector(0,0,0), l=0, color=Color(), reflection=10):
-        super().__init__(v, l, l, l, color, reflection)
+    def __init__(self, v=Vector(0,0,0), l=0, color=Color(), specular=100, reflection=0.1):
+        super().__init__(v, l, l, l, color, specular, reflection)
 
-    def intersection(self, ray):
+    def intersection(self, ray, tMin, tMax):
         bounds = list()
         bounds.append(self.minPoint)
         bounds.append(self.maxPoint)
