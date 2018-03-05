536b0d047a40dca3ca9ecf985239e7b2e9cf7870
Sebastian Köhler <sebastiankoehler2@gmx.net>
2018-03-05 22:33:38 +0000

    added surface normal

--- backend/RayTracing/Classes/Models/Cuboid.py
+++ backend/RayTracing/Classes/Models/Cuboid.py
@@ -5,8 +5,8 @@ from RayTracing.Classes.Models.Vector import Vector
 
 class Cuboid(Object3D):
 
-    def __init__(self, x=0, y=0, z=0, w=0, h=0, d=0, color=Color(), reflection=10):
-        super().__init__(x, y, z, color, reflection)
+    def __init__(self, x=0, y=0, z=0, w=0, h=0, d=0, color=Color(), specular=100, reflection=0.1):
+        super().__init__(x, y, z, color, specular, reflection)
         self.w = w;
         self.h = h;
         self.d = d;
@@ -14,8 +14,8 @@ class Cuboid(Object3D):
         self.minPoint = self.center.sub(shiftVector)
         self.maxPoint = self.center.add(shiftVector)
 
-    def __init__(self, v=Vector(0,0,0), w=0, h=0, d=0, color=Color(), reflection=10):
-        super().__init__(v, reflection)
+    def __init__(self, v=Vector(0,0,0), w=0, h=0, d=0, color=Color(), specular=100, reflection=0.1):
+        super().__init__(v, color, specular, reflection)
         self.w = w;
         self.h = h;
         self.d = d;
@@ -23,7 +23,12 @@ class Cuboid(Object3D):
         self.minPoint = self.center.sub(shiftVector)
         self.maxPoint = self.center.add(shiftVector)
 
-
+    def getSurfaceNormal(self, point):
+        surfaceNormal = Vector()
+        surfaceNormal.x = point.x - self.center.x
+        surfaceNormal.y = point.y
+        surfaceNormal.z = point.y - self.center.z
+        return surfaceNormal.normalize()
 
 
 
